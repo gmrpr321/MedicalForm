ef56cf1d00b83de239f32e3043229311a1db4e08
pradeep <pradeepsn606@gmail.com>
2023-06-23 08:01:34 +0530

    'fifth'

--- Backend/env/MedicalForm/MedicalCollegeForm/AdmissionForm/urls.py
+++ Backend/env/MedicalForm/MedicalCollegeForm/AdmissionForm/urls.py
@@ -6,6 +6,9 @@ from .views import (
     AdmissionFormCreate,
     AdmissionFormRetrieve,
     AdmissionFormUpdate,
+    CreateUser,
+    LoginUser,
+    LogoutUser,
 )
 
 urlpatterns = [
@@ -39,4 +42,7 @@ urlpatterns = [
         AdmissionFormRetrieve.as_view(),
         name="admission-retrive",
     ),
+    path("user-create", CreateUser.as_view()),
+    path("user-login", LoginUser.as_view()),
+    path("user-logout", LogoutUser.as_view()),
 ]
--- Backend/env/MedicalForm/MedicalCollegeForm/AdmissionForm/views.py
+++ Backend/env/MedicalForm/MedicalCollegeForm/AdmissionForm/views.py
@@ -3,6 +3,8 @@ from .serializers import ApplicationStudentSerializer, AdmissionStudentSerialize
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from .models import StudentModel
@@ -64,3 +66,36 @@ class AdmissionFormRetrieve(APIView):
         instance = StudentModel.objects.get(email=pk)
         serializer = AdmissionStudentSerializer(instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CreateUser(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        username = request.data.get("username")
+
+        try:
+            User.objects.create(username=username, email=email, password=password)
+            return Response(status=status.HTTP_201_CREATED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginUser(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(request, username=username, password=password, email=email)
+        if user is not None:
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutUser(APIView):
+    def get(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
